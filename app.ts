824fbca4102ea5c71cb499463d247349e14fff8a
Matt <69976995+Matt54633@users.noreply.github.com>
2024-09-30 16:17:08 +0100

    Add functions

--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
 // Variable Declarations
 
 import { Employee } from "./classes";
+import { capitalise } from "./functions";
 
 let x: number = 0;
-let forename: string = "Matt";
+let forename: string = "matt";
 let surname: string = "Sullivan"
 let fullName: string = `${forename} ${surname}`;
 let ages: number[] = [1, 2, 3, 4, 5]
@@ -29,4 +30,5 @@ ages.forEach(age => {
 let Matt: Employee = new Employee(20, "Matt", "Sullivan", new Date(), 1234, new Date('2022-07-30'))
 
 console.log(Matt);
-console.log(Matt.YearsOfService)
+console.log(Matt.YearsOfService);
+console.log(capitalise(forename));
