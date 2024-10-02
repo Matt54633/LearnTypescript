5e47d455f91b833ecf1284159cfc4341f18b55a5
Matt <69976995+Matt54633@users.noreply.github.com>
2024-10-02 13:26:38 +0100

    Add Definitely Typed

---
+++ definitelyTyped.ts
@@ -0,0 +1,7 @@
+// Definitely Typed
+
+// Some NPM packages will not be type safe due to a lack of types. 
+// Definitely Typed provides a central repository of Typescript definitions for NPM packages which do not have types
+
+// Usage
+// npm install --save-dev @types/jquery
