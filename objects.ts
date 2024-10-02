15d95451d569f52b300c846d60d64e7e8fa3b755
Matt <69976995+Matt54633@users.noreply.github.com>
2024-10-02 10:18:48 +0100

    Add Objects

---
+++ objects.ts
@@ -0,0 +1,11 @@
+export const AudiA3: { manufacturer: string, model: string, year: number } = {
+    manufacturer: "Audi",
+    model: "A3",
+    year: 2024
+};
+
+// Optional Colour Property
+export const iPhone: { modelNo: number, storage: number, colour?: string } = {
+    modelNo: 16,
+    storage: 256,
+}
