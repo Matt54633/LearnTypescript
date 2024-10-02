01cbb62b0b5c8cb3216e4af192e085677be4e8a4
Matt <69976995+Matt54633@users.noreply.github.com>
2024-10-02 10:29:41 +0100

    Add UnionTypes

---
+++ unionTypes.ts
@@ -0,0 +1,7 @@
+export function printStatusCode(code: string | number) {
+    console.log(`Status Code is ${code}`);
+}
+
+// Function could be called like:
+// printStatusCode(400);
+// printStatusCode('400');
